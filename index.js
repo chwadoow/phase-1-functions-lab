8338cbeed0f34ccd5824c6a41b9e7edcfb82c141
moses ngige <moses32ngigz@gmail.com>
2022-06-07 14:32:52 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution in this file!
+
+const distanceFromHqInBlocks = (block) => {
+    return (block >= 42) ? block - 42 : 42 - block ;
+}
+
+
+
+
+
+const distanceFromHqInFeet = (distance)=>{
+    return distanceFromHqInBlocks(distance) * 264;
+    }

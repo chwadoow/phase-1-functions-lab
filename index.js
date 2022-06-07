89e9a01fdd2c48e2ac308765a34169226c12b98d
moses ngige <moses32ngigz@gmail.com>
2022-06-07 15:41:12 +0300

    Done.

--- index.js
+++ index.js
@@ -3,11 +3,26 @@
 const distanceFromHqInBlocks = (block) => {
     return (block >= 42) ? block - 42 : 42 - block ;
 }
-
-
-
-
-
 const distanceFromHqInFeet = (distance)=>{
     return distanceFromHqInBlocks(distance) * 264;
     }
+const  distanceTravelledInFeet = (start , destination) =>{
+    return start > destination ? (start - destination) * 264 : (destination - start) * 264;
+
+
+}
+const calculatesFarePrice = (start, destination) =>{
+    let distance = distanceTravelledInFeet(start,destination)
+    if (distance <= 400 ){
+        return 0;
+    
+    }
+    else if (distance > 400 && distance <= 2000){
+        return ((distance - 400) * 0.02);
+    }
+    else if ((distance > 2000 && distance <= 2500)){
+        return 25;
+    }
+    else { return 'cannot travel that far'};
+    
+}
